4eed630f9ea37f544dd68b04b44b28928f138350
Norbert Korczyk <korczyk.norbert@gmail.com>
2020-09-13 12:39:04 +0200

    initial server setup

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 100,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+};
